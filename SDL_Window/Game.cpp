fccd47b91e0ecca030622d82b246a1e7a0008caf
Lime0333 <kominkowskikacper.zolle633@gmail.com>
2023-11-24 18:20:23 +0100

    Add project files.

---
+++ SDL_Window/Game.cpp
@@ -0,0 +1,108 @@
+#include "Game.hpp"
+
+#include"TextureManager.h"
+#include"Map.h"
+#include "ECS/Components.h"
+#include"Vector2D.h"
+#include"Collision.h"
+
+Map* map;
+Manager manager;
+
+SDL_Renderer* Game::renderer = nullptr;
+SDL_Event Game::event;
+
+std::vector<ColliderComponent*> Game::colliders;
+
+auto& player(manager.addEntity());
+auto& wall(manager.addEntity());
+
+
+Game::Game()
+{}
+
+Game::~Game()
+{}
+
+void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+{
+
+	int flags = 0;
+	if (fullscreen) {
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+		std::cout << "Subsystems Initialized!..." << std::endl;
+
+		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+		if (window) {
+			std::cout << "Window created!" << std::endl;
+		}
+
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer) {
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			std::cout << "Renderer created!" << std::endl;
+		}
+
+		isRunning = true;
+	}
+	else { isRunning = false; }
+
+	map = new Map();
+
+	Map::LoadMap("assets/bg/maps/1.map", 16, 16);
+
+	player.addComponent<TransformComponent>(2);
+	player.addComponent<SpriteComponent>("assets/player.png");
+	player.addComponent<KeyboardController>();
+	player.addComponent<ColliderComponent>("player");
+
+	wall.addComponent<TransformComponent>(300.0f, 300.0f, 300, 20, 1);
+	wall.addComponent<SpriteComponent>("assets/bg/dirt.png");
+	wall.addComponent<ColliderComponent>("wall");
+}
+
+void Game::handleEvents() {
+
+	SDL_PollEvent(&event);
+	switch (event.type) {
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		default:
+			break;
+	}
+}
+
+void Game::update() {
+
+	manager.refresh();
+	manager.update();
+
+	for (auto cc : colliders) {
+
+		Collision::AABB(player.getComponent<ColliderComponent>(), *cc);
+	}
+}
+void Game::render()
+{
+	SDL_RenderClear(renderer);
+
+	manager.draw();
+	SDL_RenderPresent(renderer);
+}
+
+void Game::clean()
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	std::cout << "Game cleaned!" << std::endl;
+}
+
+void Game::AddTile(int id, int x, int y) {
+	auto& tile(manager.addEntity());
+	tile.addComponent<TileComponent>(x, y, 32, 32, id);
+}
